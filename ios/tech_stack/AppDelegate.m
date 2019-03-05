102f48eb404a260f2093af0cfd26566ed75f444d
Mani Jafari <manielli@gmail.com>
2019-03-04 16:36:20 -0800

    installed redux using: npm install redux react-redux, also added a src folder and App.js in it and pointed the main index.js to it

--- ios/tech_stack/AppDelegate.m
+++ ios/tech_stack/AppDelegate.m
@@ -22,7 +22,7 @@
                                                       moduleName:@"tech_stack"
                                                initialProperties:nil
                                                    launchOptions:launchOptions];
-  rootView.backgroundColor = [UIColor blackColor];
+  rootView.backgroundColor = [UIColor whiteColor];
 
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
